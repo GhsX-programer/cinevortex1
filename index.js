be22b33dd1a7b00b383cae22c851241b1e2791e2
GhsX-programer <pagueghsx@gmail.com>
2024-10-29 19:07:36 -0300

    Initial commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+const Dotenv = require('dotenv').config();
 const express = require('express');
 const Sequelize = require('sequelize')
 const app = express();
-const port = 3000
+const port = process.env.PORT || 3000;
 const bodyparser = require('body-parser');
 const Bcrypt = require('bcrypt')
 const connection = require('./database/conection');
