07d24972efee5303cf259d276119188df24d3098
GhsX-programer <pagueghsx@gmail.com>
2024-10-29 23:45:20 -0300

    Initial commit

--- database/conection.js
+++ database/conection.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize('cadastrouser' , 'root' , '' , {
-    host: 'localhost',
+const sequelize = new Sequelize('vzm7miunxhsuh5fa' , 'jqug845snc1roa7p' , 's4z0cqi2uybznrcx' , {
+    host: 'nba02whlntki5w2p.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     dialect: 'mysql',
     port: 3306
 } )
